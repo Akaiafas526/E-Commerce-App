7d3331e0cb2d7455aa16b0a8db5d76859d28ac24
Andreas Kaiafas <akaiafas526@gmail.com>
2022-03-31 23:45:17 -0400

    created category model, in Category.js file

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// turns on routes
 app.use(routes);
 
+// turns on connection to my server and database
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
