ecfce9d3ca64404e65dcdf3de6145201035e8436
Andreas Kaiafas <akaiafas526@gmail.com>
2022-04-01 15:29:55 -0400

    update to index.js model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -17,14 +17,12 @@ ProductTag.init(
       references: {
         model: "product",
         key: "id",
-        unique: false,
       },
       tag_id: {
         type: DataTypes.INTEGER,
         references: {
           model: "tag",
           key: "id",
-          unique: false,
         },
       },
     },
--- models/index.js
+++ models/index.js
@@ -6,15 +6,25 @@ const ProductTag = require('./ProductTag');
 
 // Product belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: ''
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
 })
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {model:ProductTag},
+  foreignKey: 'product_id'
+ })
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: {model:ProductTag},
+  foreignKey: 'tag_id'
+ })
 module.exports = {
   Product,
   Category,
